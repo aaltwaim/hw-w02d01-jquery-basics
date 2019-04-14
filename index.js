68573bf1bdc41caf24a41aa3b7fe9a47486809a3
Abdullah Altwaim <aaltwaim@Abdullahs-MacBook-Pro.local>
2019-04-14 21:27:50 +0300

    HW done

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 // turn on stop function 
+$('#stopButton').on('click', turnOnStopLight)
 function turnOnStopLight() {
+  $('#stopLight').css('background-color','red');
+  $('#slowLight').css('background-color','black');
+  $('#goLight').css('background-color','black');
     // change background color of stop light to red
     // changes background color of green and yellow to black
 }
 
 // turn on slow function
+$('#slowButton').on('click', turnOnSlowLight)
 function turnOnSlowLight() { 
+  $('#stopLight').css('background-color','black');
+  $('#slowLight').css('background-color','yellow');
+  $('#goLight').css('background-color','black');
   // change background color of slow light to yellow
   // changes background color of red and green to black
 }
 
 // turn on go function
+$('#goButton').on('click', turnOnGoLight)
 function turnOnGoLight() {
+  $('#stopLight').css('background-color','black');
+  $('#slowLight').css('background-color','black');
+  $('#goLight').css('background-color','green');
     // change background color of go light to green
     // changes background color of red and yellow to black
 }
